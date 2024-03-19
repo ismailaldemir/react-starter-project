a60d700a299025e8bf014a0a22e30377d11fc35c
ismailaldemir <64287408+ismailaldemir@users.noreply.github.com>
2024-03-19 20:13:38 +0300

    react dersi başlangıç kodları

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Yeni başlangıçlar için
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          React Dersleri 2024
         </a>
       </header>
     </div>
